5e9c732949cb714be486f0bf42c6cb53eb4e0abf
NishantChauhan032 <nishantkr0210@gmail.com>
2020-10-13 17:41:19 +0530

    [NISHANT] add : To find maximum in given three String values & Test Cases done

--- src/main/java/com/capg/JUnit/maximum/TestMaximum/FindMaximum.java
+++ src/main/java/com/capg/JUnit/maximum/TestMaximum/FindMaximum.java
@@ -11,24 +11,42 @@ public class FindMaximum {
 
 	public static Integer findMaximumInteger(Integer number1, Integer number2, Integer number3) {
 		Integer maximumOfThree = number1;
-		if (number2.compareTo(number1) > 0)
+		if (number2.compareTo(maximumOfThree) > 0)
 			maximumOfThree = number2;
-		if (number3.compareTo(number1) > 0)
+		if (number3.compareTo(maximumOfThree) > 0)
 			maximumOfThree = number3;
-		LOG.info("Maximum Number is : " + maximumOfThree);
+		LOG.info("Maximum Number in 32,23,46 is : " + maximumOfThree);
 		return maximumOfThree;
+
+	}
+	
+	public static Float findMaximumFloat(Float float1, Float float2, Float float3) {
+		Float maximumFloatOfThree = float1;
+		if (float2.compareTo(maximumFloatOfThree) > 0)
+			maximumFloatOfThree = float2;
+		if (float3.compareTo(maximumFloatOfThree) > 0)
+			maximumFloatOfThree = float3;
+		LOG.info("Maximum Number in 2.5f,3.5f,9.5f is : " + maximumFloatOfThree);
+		return maximumFloatOfThree;
+
+	}
+	
+	public static String findMaximumString(String string1, String string2, String string3) {
+		String maximumStringOfThree = string1;
+		if (string2.compareTo(maximumStringOfThree) > 0)
+			maximumStringOfThree = string2;
+		if (string3.compareTo(maximumStringOfThree) > 0)
+			maximumStringOfThree = string3;
+		LOG.info("Maximum String in Apple,Peach,Banana is : " + maximumStringOfThree);
+		return maximumStringOfThree;
 
 	}
 
 	public static void main(String[] args) {
 
-		LOG.info("Enter Number 1 : ");
-		Integer n1 = sc.nextInt();
-		LOG.info("Enter Number 2 : ");
-		Integer n2 = sc.nextInt();
-		LOG.info("Enter Number 3 : ");
-		Integer n3 = sc.nextInt();
-		findMaximumInteger(n1, n2, n3);
+		findMaximumInteger(32, 23, 46);
+		findMaximumFloat(2.5f,3.5f,9.5f);
+		findMaximumString("Apple","Peach","Banana");
 
 	}
 }
--- src/test/java/com/capg/JUnit/maximum/TestMaximum/FindMaximumTest.java
+++ src/test/java/com/capg/JUnit/maximum/TestMaximum/FindMaximumTest.java
@@ -7,27 +7,39 @@ public class FindMaximumTest{
 	
 	@Test
 	public void givenMaximumNumber_atFirstPosition_shouldReturnSameNumber() {
-		Integer number1 = 32;
-		Integer number2 = 23;
-		Integer number3 = 29;
-		Integer maximumOfThree = FindMaximum.findMaximumInteger(number1,number2,number3);
-		Assert.assertEquals(number1, maximumOfThree);	}
+		Integer maximumOfThree = FindMaximum.findMaximumInteger(32,23,11);
+		Assert.assertEquals((Integer)32, maximumOfThree);
+	
+		Float maximumFloatOfThree = FindMaximum.findMaximumFloat(9.1f, 3.1f, 2.1f);
+		Assert.assertEquals((Float)9.1f, maximumFloatOfThree);
+		
+		String maximumStringOfThree = FindMaximum.findMaximumString("Peach","Apple","Banana");
+		Assert.assertEquals("Peach", maximumStringOfThree);
+	}
 	
 	@Test
 	public void givenMaximumNumber_atSecondPosition_shouldReturnSameNumber() {
-		Integer number1 = 32;
-		Integer number2 = 93;
-		Integer number3 = 29;
-		Integer maximumOfThree = FindMaximum.findMaximumInteger(number1,number2,number3);
-		Assert.assertEquals(number2, maximumOfThree);	}
+		Integer maximumOfThree = FindMaximum.findMaximumInteger(32,93,29);
+		Assert.assertEquals((Integer)93, maximumOfThree);
+	
+		Float maximumFloatOfThree = FindMaximum.findMaximumFloat(3.1f, 9.1f, 2.1f);
+		Assert.assertEquals((Float)9.1f, maximumFloatOfThree);
+		
+		String maximumStringOfThree = FindMaximum.findMaximumString("Apple","Peach","Banana");
+		Assert.assertEquals("Peach", maximumStringOfThree);
+	}
 	
 	@Test
 	public void givenMaximumNumber_atThirdPosition_shouldReturnSameNumber() {
-		Integer number1 = 32;
-		Integer number2 = 23;
-		Integer number3 = 92;
-		Integer maximumOfThree = FindMaximum.findMaximumInteger(number1,number2,number3);
-		Assert.assertEquals(number3, maximumOfThree);	}
+		Integer maximumOfThree = FindMaximum.findMaximumInteger(32,31,98);
+		Assert.assertEquals((Integer)98, maximumOfThree);
+	
+		Float maximumFloatOfThree = FindMaximum.findMaximumFloat(3.1f, 2.1f, 9.1f);
+		Assert.assertEquals((Float)9.1f, maximumFloatOfThree);
+		
+		String maximumStringOfThree = FindMaximum.findMaximumString("Apple","Banana","Peach");
+		Assert.assertEquals("Peach", maximumStringOfThree);
+	}
 }
 
 
