2f2740c015f5f241dd48f6333c29f0beee771bab
NishantChauhan032 <nishantkr0210@gmail.com>
2020-10-13 19:38:40 +0530

    [NISHANT] refactored : Created generic class to take in 3 variables of generic type

--- src/test/java/com/capg/JUnit/maximum/TestMaximum/FindMaximumTest.java
+++ src/test/java/com/capg/JUnit/maximum/TestMaximum/FindMaximumTest.java
@@ -3,31 +3,41 @@ package com.capg.JUnit.maximum.TestMaximum;
 import org.junit.Test;
 import org.junit.Assert;
 
-public class FindMaximumTest{
-	
+public class FindMaximumTest {
+
 	@Test
 	public void givenMaximumNumber_atFirstPosition_shouldReturnSameNumber() {
-		Integer number1 = 32;
-		Integer number2 = 23;
-		Integer number3 = 29;
-		Integer maximumOfThree = FindMaximum.findMaximumInteger(number1,number2,number3);
-		Assert.assertEquals(number1, maximumOfThree);	}
-	
+		Integer maximumOfThree = new FindMaximum<Integer>(32, 23, 11).findMax();
+		Assert.assertEquals((Integer) 32, maximumOfThree);
+
+		Float maximumFloatOfThree = new FindMaximum<Float>(9.1f, 3.1f, 2.1f).findMax();
+		Assert.assertEquals((Float) 9.1f, maximumFloatOfThree);
+
+		String maximumStringOfThree = new FindMaximum<String>("Peach", "Apple", "Banana").findMax();
+		Assert.assertEquals("Peach", maximumStringOfThree);
+	}
+
 	@Test
 	public void givenMaximumNumber_atSecondPosition_shouldReturnSameNumber() {
-		Integer number1 = 32;
-		Integer number2 = 93;
-		Integer number3 = 29;
-		Integer maximumOfThree = FindMaximum.findMaximumInteger(number1,number2,number3);
-		Assert.assertEquals(number2, maximumOfThree);	}
-	
+		Integer maximumOfThree = new FindMaximum<Integer>(32, 93, 29).findMax();
+		Assert.assertEquals((Integer) 93, maximumOfThree);
+
+		Float maximumFloatOfThree = new FindMaximum<Float>(3.1f, 9.1f, 2.1f).findMax();
+		Assert.assertEquals((Float) 9.1f, maximumFloatOfThree);
+
+		String maximumStringOfThree = new FindMaximum<String>("Apple", "Peach", "Banana").findMax();
+		Assert.assertEquals("Peach", maximumStringOfThree);
+	}
+
 	@Test
 	public void givenMaximumNumber_atThirdPosition_shouldReturnSameNumber() {
-		Integer number1 = 32;
-		Integer number2 = 23;
-		Integer number3 = 92;
-		Integer maximumOfThree = FindMaximum.findMaximumInteger(number1,number2,number3);
-		Assert.assertEquals(number3, maximumOfThree);	}
+		Integer maximumOfThree = new FindMaximum<Integer>(32, 31, 98).findMax();
+		Assert.assertEquals((Integer) 98, maximumOfThree);
+
+		Float maximumFloatOfThree = new FindMaximum<Float>(3.1f, 2.1f, 9.1f).findMax();
+		Assert.assertEquals((Float) 9.1f, maximumFloatOfThree);
+
+		String maximumStringOfThree = new FindMaximum<String>("Apple", "Banana", "Peach").findMax();
+		Assert.assertEquals("Peach", maximumStringOfThree);
+	}
 }
-
-
